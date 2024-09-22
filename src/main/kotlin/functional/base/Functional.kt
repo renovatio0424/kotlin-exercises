9f1ba7ee9bd8c3cad3a9c4bcc78cf76350fb7838
JungWon, Kim <renovatio0424@gmail.com>
2024-09-22 18:36:31 +0900

    ch5~7 연습 문제 풀이

--- src/main/kotlin/functional/base/Functional.kt
+++ src/main/kotlin/functional/base/Functional.kt
@@ -31,41 +31,61 @@ data class Name(val name: String)
 class AnonymousFunctionalTypeSpecified {
     val add: (Int, Int) -> Int = fun(num1, num2) = num1 + num2
 
-    // TODO: Implement printNum, triple, produceName and longestOf properties using anonymous functions
-    //  their type should be specified explicitly
-    //  See add property for example
+    val printNum: (Int) -> Unit = fun(num) = println(num)
+
+    val triple: (Int) -> Int = fun (num) = num * 3
+
+    val produceName: (String) -> Name = fun(name) = Name(name)
+
+    val longestOf: (String, String, String) -> String = fun(str1, str2, str3) = maxOf(str1, str2, str3, compareBy { it.length })
 }
 
 class AnonymousFunctionalTypeInferred {
     val add = fun(num1: Int, num2: Int) = num1 + num2
 
-    // TODO: Implement printNum, triple, produceName and longestOf properties using anonymous functions
-    //  their type should be inferred by compiler
-    //  See add property for example
+    val printNum = fun(num: Int) = println(num)
+
+    val triple = fun(num: Int) = num * 3
+
+    val produceName = fun(name: String) = Name(name)
+
+    val longestOf = fun(str1: String, str2: String, str3: String) = maxOf(str1, str2, str3, compareBy { it.length })
 }
 
 class LambdaFunctionalTypeSpecified {
     val add: (Int, Int) -> Int = { num1, num2 -> num1 + num2 }
 
-    // TODO: Implement printNum, triple, produceName and longestOf properties using lambda functions
-    //  their type should be specified explicitly
-    //  See add property for example
+    val printNum: (Int) -> Unit = { num -> println(num) }
+
+    val triple: (Int) -> Int = { num -> num * 3 }
+
+    val produceName: (String) -> Name = { name -> Name(name) }
+
+    val longestOf: (String, String, String) -> String = { str1, str2, str3 -> maxOf(str1, str2, str3, compareBy { it.length }) }
 }
 
 class LambdaFunctionalTypeInferred {
     val add = { num1: Int, num2: Int -> num1 + num2 }
 
-    // TODO: Implement printNum, triple, produceName and longestOf properties using lambda functions
-    //  their type should be inferred by compiler
-    //  See add property for example
+    val printNum = { num: Int -> println(num) }
+
+    val triple = { num: Int -> num * 3 }
+
+    val produceName = { name: String -> Name(name) }
+
+    val longestOf = { str1: String, str2: String, str3: String -> maxOf(str1, str2, str3, compareBy { it.length }) }
 }
 
 class LambdaUsingImplicitParameter {
     val add: (Int, Int) -> Int = { num1, num2 -> num1 + num2 }
 
-    // TODO: Implement printNum, triple, produceName and longestOf properties,
-    //  just like in LambdaFunctionalTypeSpecified, but this time, whenever possible,
-    //  use implicit parameter `it`.
+    val printNum: (Int) -> Unit = { println(it) }
+
+    val triple: (Int) -> Int = { it * 3 }
+
+    val produceName: (String) -> Name = { Name(it) }
+
+    val longestOf: (String, String, String) -> String = { str1, str2, str3 -> maxOf(str1, str2, str3, compareBy { it.length }) }
 }
 
 class FunctionalTest {
